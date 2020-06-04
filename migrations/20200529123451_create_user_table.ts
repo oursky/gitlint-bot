42b7c89e07be3988d972264c3b97ed9e5dc8105b
Arthelon <hsing.daniel@gmail.com>
2020-06-04 15:04:25 +0800

    Drop email user field
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- migrations/20200529123451_create_user_table.ts
+++ migrations/20200529123451_create_user_table.ts
@@ -5,7 +5,6 @@ const userTableName = "user";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable(userTableName, (builder) => {
     builder.increments("id").primary();
-    builder.string("email", 320).unique(); // max total length of email is 320 characters
     builder.string("name").notNullable();
   });
 }
