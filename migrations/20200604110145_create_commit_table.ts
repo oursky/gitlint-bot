67c33df8cc6235316c11955d75da461229b5d745
Arthelon <hsing.daniel@gmail.com>
2020-06-04 16:59:09 +0800

    Make commit timestamp non-nullable
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- migrations/20200604110145_create_commit_table.ts
+++ migrations/20200604110145_create_commit_table.ts
@@ -8,7 +8,7 @@ export async function up(knex: Knex): Promise<void> {
     (builder: Knex.TableBuilder) => {
       builder.text("id").notNullable().primary();
       builder.text("message").notNullable();
-      builder.timestamp("committed_at");
+      builder.timestamp("committed_at").notNullable();
       builder.integer("user_id").notNullable();
       builder.foreign("user_id").references("id").inTable("user");
     }
