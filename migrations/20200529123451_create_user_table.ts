7ab2ef88e4f9f0999ec1e577cc36d900db2f578c
Arthelon <hsing.daniel@gmail.com>
2020-06-04 11:37:01 +0800

    Add user table migration
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

---
+++ migrations/20200529123451_create_user_table.ts
@@ -0,0 +1,15 @@
+import * as Knex from "knex";
+
+const userTableName = "user";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(userTableName, (builder) => {
+    builder.increments("id").primary();
+    builder.string("email", 320).unique(); // max total length of email is 320 characters
+    builder.string("name");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(userTableName);
+}
