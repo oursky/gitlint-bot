f26bdb00cdd6f9d4067eaf890d10c9d2730b2949
Arthelon <hsing.daniel@gmail.com>
2020-06-05 15:46:14 +0800

    Separate commit processing into sep function
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/listeners.ts
+++ src/listeners.ts
@@ -1,18 +1,21 @@
 import { Context } from "probot";
 import Webhooks from "@octokit/webhooks";
-import { lintCommitMessage, LintResults } from "./lint";
+import { lintCommitMessage } from "./lint";
 import { Commit as GithubCommit } from "types/github";
+
+async function processCommit(commit: GithubCommit) {
+  const message = commit.message;
+  const lintOutput = await lintCommitMessage(message);
+  const commitScore = lintOutput.reduce(
+    (prevScore, currResult) => prevScore + currResult.score,
+    0
+  );
+  console.log(commitScore);
+}
 
 export async function onPush(
   context: Context<Webhooks.WebhookPayloadPush>
 ): Promise<void> {
   const commits = context.payload.commits;
-  const messages = commits.map((commit: GithubCommit) => commit.message);
-  const lintOutput: LintResults[] = await Promise.all(
-    messages.map(lintCommitMessage)
-  );
-  const commitScores: number[] = lintOutput.map((result: LintResults) =>
-    result.reduce((prevScore, currResult) => prevScore + currResult.score, 0)
-  );
-  console.log(commitScores);
+  await Promise.all(commits.map(processCommit));
 }
