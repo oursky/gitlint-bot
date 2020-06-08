549f10c79c28a24d3ec9a14a641ffba530e9f29e
Arthelon <hsing.daniel@gmail.com>
2020-06-08 14:20:26 +0800

    Use .endsWith func for clarity in rule
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/lint/rules/subject-no-end-period.ts
+++ src/lint/rules/subject-no-end-period.ts
@@ -5,7 +5,7 @@ export default {
   name: "subject-no-end-period",
   score: 5,
   check: (commit: Commit): RuleCheckResults => {
-    const valid = commit.header.charAt(commit.header.length - 1) !== ".";
+    const valid = !commit.header.endsWith(".");
     return valid ? null : {};
   },
 };
