d2ddd028ba2c56ff7991b2fb023c9c5b72d5cb1f
Arthelon <hsing.daniel@gmail.com>
2020-06-08 12:54:26 +0800

    Add default value for score column
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- migrations/20200605172945_add_commit_score_column.ts
+++ migrations/20200605172945_add_commit_score_column.ts
@@ -2,7 +2,7 @@ import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.table("commit", (builder) => {
-    builder.integer("score").notNullable();
+    builder.integer("score").notNullable().defaultTo(0);
   });
 }
 
