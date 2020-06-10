2d7b1a9ad09df1ab5655ce64e6b264cb3095c929
Arthelon <hsing.daniel@gmail.com>
2020-06-10 10:53:52 +0800

    Unit tests for preset list in 'lintCommitMessage' func
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/lint/index.test.ts
+++ src/lint/index.test.ts
@@ -3,31 +3,72 @@ import { lintCommitMessage } from "./";
 import fc from "fast-check";
 
 describe("'lintCommitMessage' function", () => {
-  describe("when a unicode string commit message is provided", function () {
-    it("should produce an object matching the LintResult type", async () => {
+  describe("when an empty rule preset is provided", () => {
+    it("should return 0 score and empty violations list", async () => {
       await fc.assert(
-        fc.asyncProperty(fc.unicodeString(), async (input) => {
-          const lintOutput = await lintCommitMessage(input);
-          expect(lintOutput).toBeDefined();
-          expect(lintOutput).toEqual({
-            score: expect.any(Number),
-            violations: expect.arrayContaining([
-              {
-                ruleName: expect.any(String),
-                violation: expect.any(Object),
-              },
-            ]),
+        fc.asyncProperty(fc.fullUnicodeString(), async (commitMessage) => {
+          const lintOutput = await lintCommitMessage(commitMessage, []);
+          expect(lintOutput).toMatchObject({
+            score: 0,
+            violations: [],
           });
         })
       );
     });
+  });
 
-    it("should return a score greater than or equal to 0", async () => {
+  describe("when a non-empty rule preset is provided", () => {
+    it("should call every rule 'check' function once", async () => {
       await fc.assert(
-        fc.asyncProperty(fc.unicodeString(), async (input) => {
-          const lintOutput = await lintCommitMessage(input);
-          expect(lintOutput.score).toBeGreaterThanOrEqual(0);
-        })
+        fc.asyncProperty(
+          fc.array(fc.fullUnicodeString()),
+          fc.fullUnicodeString(),
+          async (ruleNames, commitMessage) => {
+            const checkMock = jest.fn();
+            const preset = ruleNames.map((name) => ({
+              name,
+              score: 0,
+              check: checkMock,
+            }));
+            await lintCommitMessage(commitMessage, preset);
+
+            expect(checkMock.mock.calls.length).toEqual(preset.length);
+          }
+        )
+      );
+    });
+
+    it("should only sum scores of rules when 'check' func returns violation", async () => {
+      await fc.assert(
+        fc.asyncProperty(
+          fc.array(
+            fc.constantFrom(
+              fc.object({
+                key: fc.fullUnicodeString(),
+              }),
+              null
+            )
+          ),
+          fc.fullUnicodeString(),
+          async (returnValues, commitMessage) => {
+            const checkMock = jest.fn();
+            returnValues.forEach((mockReturnValue) => {
+              checkMock.mockReturnValueOnce(mockReturnValue);
+            });
+            const preset = returnValues.map(() => ({
+              check: checkMock,
+              score: 1,
+              name: "sample-rule-name",
+            }));
+            const { score } = await lintCommitMessage(commitMessage, preset);
+            const expectedScore = returnValues.reduce<number>(
+              (prev, curr) => (curr !== null ? prev + 1 : prev),
+              0
+            );
+
+            expect(score).toEqual(expectedScore);
+          }
+        )
       );
     });
   });
