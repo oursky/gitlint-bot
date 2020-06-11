a356a5365c739e65d048d144ba34598fa85d9904
Arthelon <hsing.daniel@gmail.com>
2020-06-11 15:36:02 +0800

    Move knexfile and db files into src/ folder
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,5 @@
 import knex from "knex";
-import knexfile from "../knexfile";
+import knexfile from "./knexfile";
 
 const env = process.env.NODE_ENV ?? "development";
 const db = knex(knexfile[env]);
