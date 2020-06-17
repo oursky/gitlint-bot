4b9ad7464904decd31d66027b7f209fdb8f248d7
Arthelon <hsing.daniel@gmail.com>
2020-06-17 12:51:14 +0800

    Schedule slack job using node-cron
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import { Application } from "probot";
 import { onPush } from "./listeners";
+import { slackJob } from "./jobs";
+import cron from "node-cron";
+
+if (typeof process.env.SLACK_CRON_PATTERN !== "string") {
+  throw new Error("Missing 'SLACK_CRON_PATTERN' env variable");
+}
+const SLACK_CRON_PATTERN = process.env.SLACK_CRON_PATTERN;
 
 export = (app: Application) => {
   app.on("push", onPush);
+
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  cron.schedule(SLACK_CRON_PATTERN, slackJob, {
+    // node-cron v2+ allows tasks to return Promises: https://github.com/node-cron/node-cron/releases/tag/v2.0.0
+    scheduled: true,
+    timezone: "Asia/Hong_Kong",
+  });
 };
---
+++ src/jobs.ts
@@ -0,0 +1,1 @@
+export async function slackJob(): Promise<void> {}
