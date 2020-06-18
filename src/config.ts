b5860e4da0e1da9a61d2363430a518d4c67b5a44
Arthelon <hsing.daniel@gmail.com>
2020-06-18 10:36:16 +0800

    Better integer env var validation
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,19 @@
-function getEnvVariable(varName: string): string {
+function getEnvString(varName: string): string {
   const envVar = process.env[varName];
   if (typeof envVar !== "string") {
     throw new Error(`Missing '${varName}' env variable`);
   }
   return envVar;
 }
-export const SLACK_CRON_PATTERN = getEnvVariable("SLACK_CRON_PATTERN");
-export const SLACK_WEBHOOK_URL = getEnvVariable("SLACK_WEBHOOK_URL");
-export const SLACK_DAY_INTERVAL = parseInt(
-  getEnvVariable("SLACK_DAY_INTERVAL"),
-  10
-); // Number of days in between successive Slack summary messages
+
+function getEnvInteger(varName: string): number {
+  const parsedValue = Number(getEnvString(varName));
+  if (Number.isNaN(parsedValue) || !Number.isSafeInteger(parsedValue)) {
+    throw new Error(`'${varName}' env variable must be an integer`);
+  }
+  return parsedValue;
+}
+
+export const SLACK_CRON_PATTERN = getEnvString("SLACK_CRON_PATTERN");
+export const SLACK_WEBHOOK_URL = getEnvString("SLACK_WEBHOOK_URL");
+export const SLACK_DAY_INTERVAL = getEnvInteger("SLACK_DAY_INTERVAL");
