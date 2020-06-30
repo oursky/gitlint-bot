04af38d8248a134749f7223c25b649fa5d5519e3
Arthelon <hsing.daniel@gmail.com>
2020-06-30 17:18:49 +0800

    Increase max length of subject to 80
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- tests/rules/subject-max-length/rule.test.ts
+++ tests/rules/subject-max-length/rule.test.ts
@@ -3,13 +3,13 @@ import rule from "../../../src/lint/rules/subject-max-length";
 import { assertRuleCheck } from "../helper";
 
 describe("'subject-max-length' rule", () => {
-  describe("when commit subject is <= 50 characters", () => {
+  describe("when commit subject is <= 80 characters", () => {
     it("should return null", async () => {
       await assertRuleCheck(path.join(__dirname, "less-than-max"), rule.check);
     });
   });
 
-  describe("when commit subject is > 50 characters", () => {
+  describe("when commit subject is > 80 characters", () => {
     it("should return a violation", async () => {
       await assertRuleCheck(path.join(__dirname, "more-than-max"), rule.check);
     });
