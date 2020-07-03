abafbd835fa826ac4de17f694503889748f002ee
Arthelon <hsing.daniel@gmail.com>
2020-07-03 14:38:34 +0800

    Add 'commands' route for slash commands
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,14 @@ setupSentry();
 import { Application } from "probot";
 import { onPush } from "./listeners";
 import { slackJob } from "./jobs";
+import slackCommandsRoutes from "./slack/commands";
 import { SLACK_CRON_PATTERN } from "./config";
 import cron from "node-cron";
 
 export = (app: Application) => {
+  const router = app.route("/");
+  router.use("/commands", slackCommandsRoutes);
+
   app.on("push", onPush);
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   cron.schedule(SLACK_CRON_PATTERN, slackJob, {
