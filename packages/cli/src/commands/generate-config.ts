c801890f23e03490d36ed614f254b3ff892c79c5
Arthelon <hsing.daniel@gmail.com>
2020-07-15 11:27:00 +0800

    Generate-config cli command generates config file from default preset
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- packages/cli/src/commands/generate-config.ts
+++ packages/cli/src/commands/generate-config.ts
@@ -1,3 +1,24 @@
-function generateConfig(): void {}
+import path from "path";
+import fs from "fs";
+
+const configTemplate = `preset: default
+rules:
+  body-max-line-length: [on, null, 80]
+  body-no-trailing-whitespace: [on]
+  subject-capitalize-first: [on]
+  subject-max-length: [on, null, 80]
+  subject-min-length: [on, null, 10]
+  subject-no-hard-tab: [on]
+  subject-no-trailing-punctuation: [on]
+  subject-no-trailing-whitespace: [on]
+`;
+
+const configFileName = ".gitlintrc";
+
+function generateConfig(): void {
+  const destFilePath = path.resolve("./", configFileName);
+  fs.writeFileSync(destFilePath, configTemplate);
+  console.log("Successfully created config file at: " + destFilePath);
+}
 
 export default generateConfig;
