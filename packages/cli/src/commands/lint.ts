941d32d75b0a5541cddee2b94d1abcc2f4b003f0
Arthelon <hsing.daniel@gmail.com>
2020-07-15 16:50:18 +0800

    Read commit messages from stdin
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- packages/cli/src/commands/lint.ts
+++ packages/cli/src/commands/lint.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
+import getStdin from "get-stdin";
 import { LintCommandFlags } from "../types";
+import { lintCommitMessage } from "@oursky/gitlint";
 import { discoverConfig, applyPresets } from "@oursky/gitlint/lib/config";
 import { RulesPreset } from "@oursky/gitlint/lib/config/schema";
 
@@ -26,9 +28,26 @@ async function loadPreset(
   return applyPresets(config);
 }
 
+async function loadCommitMessages(flags: LintCommandFlags): Promise<string[]> {
+  if (typeof flags.stdIn !== "undefined" && !!flags.stdIn) {
+    const message = await getStdin();
+    if (message.length === 0) return [];
+    return [message];
+  }
+  return [];
+}
+
 async function lintCommand(flags: LintCommandFlags): Promise<void> {
   const preset = await loadPreset(flags.config);
-  console.log(preset);
+  const messages = await loadCommitMessages(flags);
+  if (messages.length === 0) {
+    console.log("No commit messages found!");
+  }
+  for (const message of messages) {
+    console.log(message);
+    const output = await lintCommitMessage(message, preset);
+    console.log(JSON.stringify(output, null, 1));
+  }
 }
 
 export default lintCommand;
