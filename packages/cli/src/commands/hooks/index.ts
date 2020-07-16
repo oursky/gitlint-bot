649796df6a68cc882cf8b8f623c4902ea5efae53
Arthelon <hsing.daniel@gmail.com>
2020-07-16 14:32:29 +0800

    Add command for uninstall git hook
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- packages/cli/src/commands/hooks/index.ts
+++ packages/cli/src/commands/hooks/index.ts
@@ -2,15 +2,19 @@ import fs from "fs";
 import path from "path";
 import childProcess from "child_process";
 
-// const hookIdentifier = "### gitlint-cli commit-msg hook ###";
+const hookIdentifier = "### gitlint-cli commit-msg hook ###";
 const hookSrcPath = path.resolve(__dirname, "../../files/commit-msg");
 const hookDestFilename = "commit-msg";
 
-export function installHook(): void {
-  const hooksPath = childProcess
+function getHooksPath() {
+  return childProcess
     .execSync("git rev-parse --git-path hooks")
     .toString()
     .trim(); // strips newline character
+}
+
+export function installHook(): void {
+  const hooksPath = getHooksPath();
   const destPath = path.resolve(hooksPath, hookDestFilename);
   if (fs.existsSync(destPath)) {
     throw new Error(
@@ -22,4 +26,19 @@ export function installHook(): void {
   console.log("Successfully installed commit-msg hook at " + destPath);
 }
 
-export function uninstallHook(): void {}
+export function uninstallHook(): void {
+  const hooksPath = getHooksPath();
+  const destPath = path.resolve(hooksPath, hookDestFilename);
+  if (!fs.existsSync(destPath)) {
+    throw new Error(`There is no commit-msg hook at ${destPath}`);
+  }
+  const fileContents = fs.readFileSync(destPath, "utf-8");
+  const lines = fileContents.split("\n");
+  if (lines.length < 2 || lines[1] !== hookIdentifier) {
+    throw new Error(
+      `The commit-msg hook at ${destPath} was not created by gitlint-cli`
+    );
+  }
+  fs.unlinkSync(destPath);
+  console.log("Successfully uninstalled commit-msg hook at " + destPath);
+}
