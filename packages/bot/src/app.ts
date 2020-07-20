7ac47d6bfc53b9a1d897d7658bddbf8bbc73a7e1
Arthelon <hsing.daniel@gmail.com>
2020-07-20 13:55:10 +0800

    Refactor auth code into server module
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- packages/bot/src/app.ts
+++ packages/bot/src/app.ts
@@ -2,23 +2,12 @@ import { setupSentry } from "./sentry";
 setupSentry();
 
 import { Application } from "probot";
-import { Router } from "express";
 import { onPush } from "./github/listeners";
 import { summaryJob } from "./slack/jobs";
-import slackCommandsRoutes from "./slack/commands";
-import dashboardRoutes from "./dashboard/routes";
 import { SLACK_CRON_PATTERN } from "./env";
 import cron from "node-cron";
 
-function setupRoutes(router: Router): void {
-  router.use("/commands", slackCommandsRoutes);
-  router.use("/dashboard", dashboardRoutes);
-  router.get("/", (_, res) => res.redirect("/dashboard"));
-}
-
 export = (app: Application): void => {
-  setupRoutes(app.route("/"));
-
   app.on("push", onPush);
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   cron.schedule(SLACK_CRON_PATTERN, summaryJob, {
--- packages/bot/src/index.ts
+++ packages/bot/src/index.ts
@@ -1,7 +1,17 @@
 import http from "http";
 import { createProbot } from "probot";
-import { APP_ID, WEBHOOK_SECRET, PRIVATE_KEY } from "./env";
+import session from "express-session";
+import passport from "passport";
+import {
+  APP_ID,
+  WEBHOOK_SECRET,
+  PRIVATE_KEY,
+  DASHBOARD_SESSION_SECRET,
+} from "./env";
 import app from "./app";
+import slackCommandsRoutes from "./slack/commands";
+import dashboardRoutes from "./dashboard/routes";
+import "./setupAuth";
 
 const serverPort = 3000;
 
@@ -14,9 +24,24 @@ const probot = createProbot({
 
 probot.load(app);
 
-const expressServer = probot.server;
+const server = probot.server;
 
-const server = http.createServer(expressServer);
-server.listen(serverPort, () => {
+server.use(
+  session({
+    secret: DASHBOARD_SESSION_SECRET,
+    resave: false,
+    cookie: { secure: true },
+  })
+);
+server.use(passport.initialize());
+server.use(passport.session());
+
+// Routes
+server.use("/commands", slackCommandsRoutes);
+server.use("/dashboard", dashboardRoutes);
+server.get("/", (_, res) => res.redirect("/dashboard"));
+
+const httpServer = http.createServer(server);
+httpServer.listen(serverPort, () => {
   probot.logger.info(`Server listening on port: ${serverPort}`);
 });
