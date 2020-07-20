66c6a04b34f8c5d621dbaeee265f524b8a91a7e3
Arthelon <hsing.daniel@gmail.com>
2020-07-20 12:49:11 +0800

    Initialize passport with google oauth2 strategy
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- packages/bot/src/dashboard/routes.ts
+++ packages/bot/src/dashboard/routes.ts
@@ -1,7 +1,20 @@
 import { Router } from "express";
+import passport from "passport";
+import session from "express-session";
+import { DASHBOARD_SESSION_SECRET } from "../env";
+import "./setupAuth";
 
 const router = Router();
 
+router.use(
+  session({
+    secret: DASHBOARD_SESSION_SECRET,
+    resave: false,
+    cookie: { secure: true },
+  })
+);
+router.use(passport.initialize());
+router.use(passport.session());
 router.get("/", (_, res) => {
   res.send("Boilerplate Dashboard Route");
 });
