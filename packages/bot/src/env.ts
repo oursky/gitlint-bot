ac191769e72f01da2095ed3bff06ec371df42562
Arthelon <hsing.daniel@gmail.com>
2020-07-20 10:25:30 +0800

    Add google oauth creds to bot environment vars
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- packages/bot/src/env.ts
+++ packages/bot/src/env.ts
@@ -18,3 +18,5 @@ export const SLACK_SIGNING_SECRET = getEnvString("SLACK_SIGNING_SECRET");
 export const SLACK_CRON_PATTERN = getEnvString("SLACK_CRON_PATTERN");
 export const SLACK_WEBHOOK_URL = getEnvString("SLACK_WEBHOOK_URL");
 export const SLACK_DAY_INTERVAL = getEnvInteger("SLACK_DAY_INTERVAL");
+export const GOOGLE_CLIENT_ID = getEnvString("GOOGLE_CLIENT_ID");
+export const GOOGLE_CLIENT_SECRET = getEnvString("GOOGLE_CLIENT_SECRET");
